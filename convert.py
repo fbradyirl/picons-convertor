3c4787e2aaff0bf1126fb5e60d7f0f667c69d513
Finbarr Brady <fibrady@cisco.com>
2020-09-14 15:36:54 +0100

    Update

--- convert.py
+++ convert.py
@@ -1,6 +1,4 @@
 
 import cairosvg
 
-# >>> cairosvg.svg2pdf(url='image.svg', write_to='image.pdf')
-
-cairosvg.svg2png(url='picons-source/build-source/logos/rte2.default.svg', write_to='rte2.png')
+cairosvg.svg2png(url='picons-source/build-source/logos/rte2.default.svg', write_to='picons/rte2.png')
